282110ca593dd4158bec32b3fcca0b6b171dd6cf
yutatsu.oe <yutatsu0119@gmail.com>
2022-02-23 18:31:05 +0900

    update

--- s3-object-lambda/bin/s3-object-lambda.ts
+++ s3-object-lambda/bin/s3-object-lambda.ts
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { S3ObjectLambdaStack } from '../lib/s3-object-lambda-stack';
+import * as cdk from "aws-cdk-lib";
+import { S3ObjectLambdaStack } from "../lib/s3-object-lambda-stack";
 
 const app = new cdk.App();
-new S3ObjectLambdaStack(app, 'S3ObjectLambdaStack', {
+new S3ObjectLambdaStack(app, "S3ObjectLambdaStack", {
+  env: {
+    account: app.node.tryGetContext("accountId"),
+    /**
+     * Stack must be in us-east-1, because the ACM certificate for a
+     * global CloudFront distribution must be requested in us-east-1.
+     */
+    region: "us-east-1",
+  },
 });
